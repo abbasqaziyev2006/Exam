a087d79014c01d7d6181eb3f164a67648504e8ee
mahammad <maqa70926@gmail.com>
2025-05-10 14:55:32 +0400

    pushCommity

---
+++ Exam.Design/Form1.Designer.cs
@@ -0,0 +1,242 @@
+﻿namespace Exam.Design
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            button2 = new Button();
+            textBox1 = new TextBox();
+            button3 = new Button();
+            textBox2 = new TextBox();
+            label1 = new Label();
+            checkBox1 = new CheckBox();
+            label2 = new Label();
+            linkLabel1 = new LinkLabel();
+            pictureBox2 = new PictureBox();
+            button1 = new Button();
+            pictureBox1 = new PictureBox();
+            pictureBox3 = new PictureBox();
+            sqlCommand1 = new Microsoft.Data.SqlClient.SqlCommand();
+            label3 = new Label();
+            label4 = new Label();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
+            SuspendLayout();
+            // 
+            // button2
+            // 
+            button2.FlatStyle = FlatStyle.System;
+            button2.ForeColor = Color.FromArgb(0, 192, 0);
+            button2.Location = new Point(486, 371);
+            button2.Name = "button2";
+            button2.Size = new Size(112, 34);
+            button2.TabIndex = 1;
+            button2.Text = "Login";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(532, 226);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(150, 31);
+            textBox1.TabIndex = 2;
+            // 
+            // button3
+            // 
+            button3.BackColor = Color.Red;
+            button3.Location = new Point(998, -6);
+            button3.Name = "button3";
+            button3.Size = new Size(55, 34);
+            button3.TabIndex = 3;
+            button3.Text = "X";
+            button3.UseVisualStyleBackColor = false;
+            button3.Click += button3_Click;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(532, 151);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(150, 31);
+            textBox2.TabIndex = 4;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Impact", 10F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label1.ForeColor = SystemColors.ButtonHighlight;
+            label1.Location = new Point(417, 226);
+            label1.Name = "label1";
+            label1.Size = new Size(90, 25);
+            label1.TabIndex = 5;
+            label1.Text = "Password";
+            label1.Click += label1_Click;
+            // 
+            // checkBox1
+            // 
+            checkBox1.AutoSize = true;
+            checkBox1.Location = new Point(561, 263);
+            checkBox1.Name = "checkBox1";
+            checkBox1.Size = new Size(121, 29);
+            checkBox1.TabIndex = 6;
+            checkBox1.Text = "checkBox1";
+            checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Impact", 10F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label2.ForeColor = Color.Transparent;
+            label2.Location = new Point(417, 151);
+            label2.Name = "label2";
+            label2.Size = new Size(94, 25);
+            label2.TabIndex = 7;
+            label2.Text = "Username";
+            label2.Click += label2_Click;
+            // 
+            // linkLabel1
+            // 
+            linkLabel1.AutoSize = true;
+            linkLabel1.LinkColor = Color.Black;
+            linkLabel1.Location = new Point(582, 444);
+            linkLabel1.Name = "linkLabel1";
+            linkLabel1.Size = new Size(144, 25);
+            linkLabel1.TabIndex = 8;
+            linkLabel1.TabStop = true;
+            linkLabel1.Text = "Forget Password";
+            linkLabel1.LinkClicked += linkLabel1_LinkClicked;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Image = (Image)resources.GetObject("pictureBox2.Image");
+            pictureBox2.Location = new Point(349, 131);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(75, 57);
+            pictureBox2.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox2.TabIndex = 10;
+            pictureBox2.TabStop = false;
+            pictureBox2.Click += pictureBox2_Click;
+            // 
+            // button1
+            // 
+            button1.BackColor = SystemColors.ControlLightLight;
+            button1.FlatStyle = FlatStyle.Flat;
+            button1.ForeColor = Color.Red;
+            button1.Location = new Point(614, 371);
+            button1.Name = "button1";
+            button1.Size = new Size(112, 34);
+            button1.TabIndex = 11;
+            button1.Text = "Cancel";
+            button1.UseVisualStyleBackColor = false;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(-11, -6);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(1064, 715);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.TabIndex = 9;
+            pictureBox1.TabStop = false;
+            pictureBox1.Click += pictureBox1_Click;
+            // 
+            // pictureBox3
+            // 
+            pictureBox3.Image = (Image)resources.GetObject("pictureBox3.Image");
+            pictureBox3.Location = new Point(349, 211);
+            pictureBox3.Name = "pictureBox3";
+            pictureBox3.Size = new Size(75, 57);
+            pictureBox3.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox3.TabIndex = 12;
+            pictureBox3.TabStop = false;
+            // 
+            // sqlCommand1
+            // 
+            sqlCommand1.CommandTimeout = 30;
+            sqlCommand1.EnableOptimizedParameterBinding = false;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(0, 2);
+            label3.Name = "label3";
+            label3.Size = new Size(59, 25);
+            label3.TabIndex = 13;
+            label3.Text = "label3";
+           
+           
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.ControlDark;
+            ClientSize = new Size(1056, 710);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(pictureBox3);
+            Controls.Add(button1);
+            Controls.Add(pictureBox2);
+            Controls.Add(linkLabel1);
+            Controls.Add(label2);
+            Controls.Add(checkBox1);
+            Controls.Add(label1);
+            Controls.Add(textBox2);
+            Controls.Add(button3);
+            Controls.Add(textBox1);
+            Controls.Add(button2);
+            Controls.Add(pictureBox1);
+            Name = "Form1";
+            Text = "Login Form";
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button2;
+        private TextBox textBox1;
+        private Button button3;
+        private TextBox textBox2;
+        private Label label1;
+        private CheckBox checkBox1;
+        private Label label2;
+        private LinkLabel linkLabel1;
+        private PictureBox pictureBox2;
+        private Button button1;
+        private PictureBox pictureBox1;
+        private PictureBox pictureBox3;
+        private Microsoft.Data.SqlClient.SqlCommand sqlCommand1;
+        private Label label3;
+        private Label label4;
+    }
+}
